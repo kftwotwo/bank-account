dc8f3950b45ba6ca91ceb700e4d9ce95043bb16d
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-01 20:45:19 -0700

    add withdrawl functioin"

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
-function Account(userName, firstDeposit, secDeposit, withdrawl) {
+function Account(userName, money) {
   this.userName = userName;
-  this.firstDeposit = firstDeposit;
-  this.balance = balance;
+  this.balance = money;
 }
-
-
-
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
@@ -14,11 +10,21 @@ $(document).ready(function(){
     var firstDeposit = $("#firstDeposit").val();
     var secDeposit = $("#secDeposit").val();
     var withdrawl = $("#withdrawl").val();
-
-    var newAccount = new Account(userName, firstDeposit, secDeposit);
-    //this for "output or return"
+    if (firstDeposit === "") {
+      firstDeposit = 0;
+    }
+    if (secDeposit === "") {
+      secDeposit = 0;
+    }
+    if (withdrawl === "") {
+      withdrawl = 0;
+    }
+    var newAccount = new Account(userName, parseFloat(firstDeposit));
+    console.log(newAccount.balance)
+    newAccount.balance += parseFloat(secDeposit);
+    newAccount.balance -= parseFloat(withdrawl);
     $("#userName").text(newAccount.userName);
-    $("#balance").text(newAccount.firstDeposit);
+    $("#balance").text(newAccount.balance.toFixed(2));
 
 
   })
