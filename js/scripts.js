92dd149d8fd07cb4fbef9bffe443b9b4fecfe5f3
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-01 19:18:13 -0700

    add input for name to display

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
-function Account(userName, firstDeposit) {
+function Account(userName, firstDeposit, secDeposit, withdrawl) {
   this.userName = userName;
   this.firstDeposit = firstDeposit;
+  this.balance = balance;
 }
 
-var userName = $("#name").val();
-var firstDeposit = $("#firstDeposit").val();
 
-var newAccount = new Account(userName, firstDeposit);
 
 $(document).ready(function(){
-  ("form").submit(function(event){
+  $("form").submit(function(event){
     event.preventDefault();
 
+    var userName = $("#name").val();
+    var firstDeposit = $("#firstDeposit").val();
+    var secDeposit = $("#secDeposit").val();
+    var withdrawl = $("#withdrawl").val();
+
+    var newAccount = new Account(userName, firstDeposit, secDeposit);
+    //this for "output or return"
+    $("#userName").text(newAccount.userName);
+    $("#balance").text(newAccount.firstDeposit);
 
 
   })
