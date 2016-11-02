3662020e5d01999facc82af3a1930cecd519796e
Kevin A. Finley <kf.two.two@gmail.com>
2016-11-01 18:50:16 -0700

    add jquery and just the begining of the object

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,17 @@ function Account(userName, firstDeposit) {
   this.userName = userName;
   this.firstDeposit = firstDeposit;
 }
+
+var userName = $("#name").val();
+var firstDeposit = $("#firstDeposit").val();
+
+var newAccount = new Account(userName, firstDeposit);
+
+$(document).ready(function(){
+  ("form").submit(function(event){
+    event.preventDefault();
+
+
+
+  })
+})
